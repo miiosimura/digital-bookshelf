4305ea39b45092ffe413f36119a9ba8db9ba3a1e
mirian iosimura <mirian@blumpa.com>
2021-02-07 10:33:56 -0300

    Adds index and show for Books

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'books#index'
+  resources :books, only: [:show]
 end
